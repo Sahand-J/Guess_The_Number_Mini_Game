ef781687d6ac783530986642d85e440b095ad09b
Sahand-J <sahandjanpass@gmail.com>
2021-03-24 12:04:16 -0700

    update

--- Guess_The_Number_Mini_Game.py
+++ Guess_The_Number_Mini_Game.py
@@ -1,29 +1,47 @@
-#guess the number program
+
+"""
+Guess the number!
+
+Created on Aug 11, 2020
+@author: Sahand-j
+Refrences Automate the Boring stuff with Python Book example
+"""
 
 import random
 
 print('Hello, what is your name?')
 name = input()
 print('Hi '+str(name) + ', im thinking of a number between 1 and 20, what am thinking?')
+
 def guessTheNumber():
-    secretNumber = random.randint(1,20)
+    """
+    Main method for guessing the number game
+    :@return(none): prints output messages based on users choice 
+    """
+    secretNumber = random.randint(1,20) # randomly generated number 
     try:
-        for numberOfGuesses in range(1,7):
+        
+        for numberOfGuesses in range(1,7): # user gets 6 tries
+            
             guess = int(input())
             if guess < secretNumber:
                 print('Your guess is too LOW, Take another guess')
 
             elif guess > secretNumber:
                 print('Your guess is too HIGH, Take another guess')
-
+            
             else:
                 break #is the correct guess
+        
         if guess == secretNumber:
             print('Good job '+name+' it took you '+ str(numberOfGuesses) +' tries.')
+        
         else:
             print('Nope, the number i was thinking of was '+ str(secretNumber))
+    
     except:
         print('please enter a valid number!')
         guessTheNumber()
-guessTheNumber()
+
+guessTheNumber() # start of program
 
